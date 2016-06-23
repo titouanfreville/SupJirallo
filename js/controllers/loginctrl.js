a716dcd1dfccf210516ec6afa26e69c6e30f9987
titouanfreville <titouanfreville@hotmail.fr>
2016-06-23 22:35:32 +0200

    U

--- js/controllers/loginctrl.js
+++ js/controllers/loginctrl.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var config = require('config.json');
+// var config = require('config.json');
 
 router.get('/', function (req, res) {
     // log user out
--- js/controllers/registerctrl.js
+++ js/controllers/registerctrl.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var config = require('config.json');
+// var config = require('config.json');
 
 router.get('/', function (req, res) {
     res.render('register');
